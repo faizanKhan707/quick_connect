d5e42273e33a1b4bd1cc818fcee2ac2b906b526d
faizankhan707 <khan.faizan5@mahindra.com>
2023-09-27 16:05:21 +0530

    backup

---
+++ app/src/main/java/com/example/quickconnect/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.quickconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.net.wifi.p2p.WifiP2pManager
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
